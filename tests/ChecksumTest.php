83142ba9162158d19318ade223e6b5ecbef1f957
aju <aju.webandcrafts@gmail.com>
2020-05-10 19:29:03 +0530

    can add user defined fields for hash

--- tests/ChecksumTest.php
+++ tests/ChecksumTest.php
@@ -29,6 +29,31 @@ class ChecksumTest extends TestCase
             $hash->getStringFromHashGenerated());
     }
 
+    function testCanAddFiveUserdefinedParametersToGenerateHash()
+    {
+        $hash = Checksum::create([
+            'udf1'        => 'udf one',
+            'udf2'        => 'udf two',
+            'udf3'        => 'udf three',
+            'udf4'        => 'udf four',
+            'udf5'        => 'udf five',
+            "merchant_id" => "testMerchantId",
+            "secret_key"  => "testSecret",
+            "test_mode"   => true,
+            'txnid'       => 'zcvnlfjdkf324',
+            'amount'      => 12.00,
+            'productinfo' => 'Iphone',
+            'firstname'   => 'Jon Doe',
+            'email'       => 'jon@mail.com',
+            'phone'       => '9895309090',
+            'surl'        => 'https://example.com/success',
+            'furl'        => 'https://example.com/failure',
+        ]);
+
+        $this->assertEquals('testMerchantId|zcvnlfjdkf324|12|Iphone|Jon Doe|jon@mail.com|udf one|udf two|udf three|udf four|udf five||||||testSecret',
+            $hash->getStringFromHashGenerated());
+    }
+
     function testTransactionIdIsRequiredToCreateHash()
     {
         $this->expectException(InvalidArgumentException::class);
